8d602cbc0bc4687fbcf68493b42f689b31a5754e
QuiqueCode <dylanbusisness@gmail.com>
2024-01-11 11:01:08 -0600

    Categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import BasicButtons from './assets/components/Categories/categorias'
+import BasicButtons from './assets/components/Categories/City'
 
 function App() {
   const [count, setCount] = useState(0)
