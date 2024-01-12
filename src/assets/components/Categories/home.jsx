1edbc644f5ccdec2b580badfe29292c66a0315be
PerPai <estebanshar829@gmail.com>
2024-01-11 21:09:19 -0600

    home perfect

--- src/assets/components/Categories/home.jsx
+++ src/assets/components/Categories/home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
-import { CategoriesFeed } from './assets/components/Categories/categories';
+import { CategoriesFeed } from '../Categories/categories';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './assets/components/Categories/navbar';
-import Footer from './assets/components/Categories/footer';
+import Navbar from './navbar';
+import Footer from './footer';
 
 function Home() {
   return (
-    <Router>
+
       <div>
         <Navbar />
         <CategoriesFeed />
         <Footer />
       </div>
-    </Router>
+
   );
 }
 
